c13c64f1bbf54e837bc7981fab1d375d6950e81c
OlajideWorld <olajideoken@gmail.com>
2025-01-02 23:22:11 +0100

    first push

---
+++ contracts/Fundme.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "contracts/PriceConverter.sol";
+
+contract Fundme {
+    using PriceConverter for uint256;
+
+    address private immutable Owner;
+
+    uint256 constant MINIMUM_AMOUNT = 50 * 1e18;
+
+    mapping(address => uint256) public funderToAmountFunded;
+
+    address[] public fundersList;
+
+    constructor() {
+        Owner = msg.sender;
+    }
+
+    modifier checkOwner() {
+        require(msg.sender == Owner, "You cannot get the funds");
+        _;
+    }
+
+    function fund() public payable {
+        require(
+            msg.value.getConverter() >= MINIMUM_AMOUNT,
+            "Sorry the minimum amount is 50 USD"
+        );
+        funderToAmountFunded[msg.sender] = msg.value;
+        fundersList.push(msg.sender);
+    }
+
+    function withdraw() public checkOwner {
+        for (uint i = 0; i < fundersList.length; i++) {
+            address funderDetails = fundersList[i];
+            funderToAmountFunded[funderDetails] = 0;
+        }
+
+        fundersList = new address[](0);
+
+        // Transfer Method (not recommended)
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // Send Method (not recommended)
+        // bool success = payable(msg.sender).send(address(this).balance);
+
+        // Call Method
+        (bool callsuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callsuccess, "Unable to send to address");
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+}
---
+++ contracts/PriceConverter.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+    function decimals() internal view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+        return priceFeed.decimals();
+    }
+
+    function getPrice() internal view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+
+        (
+            ,
+            /* uint80 roundID */ int price /*uint startedAt*/ /*uint timeStamp*/ /*uint80 answeredInRound*/,
+            ,
+            ,
+
+        ) = priceFeed.latestRoundData();
+
+        //3351.47530000
+        // 3351.475300000000000000
+        return uint256(price * 1e10);
+    }
+
+    function getConverter(uint256 amount) internal view returns (uint256) {
+        uint256 usdValue = getPrice();
+        uint256 ethConverted = (usdValue * amount) / 1e18;
+
+        return ethConverted;
+    }
+}
